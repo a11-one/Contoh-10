9a345160f8373a48f962edb26fe3a8ed1c748148
a11-one <a11_one@yahoo.com>
2020-09-02 10:53:19 +0700

    Initial Commit

---
+++ Contoh 10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contoh 10
+//
+//  Created by Aldowan Apta Premala on 02/09/20.
+//  Copyright © 2020 Aldowan Apta Premala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
